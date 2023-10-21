db9d339d3a5a8d7a686b7285282b742feffb48bd
Marcus Reis <mvsantosreis@gmail.com>
2023-10-21 08:46:41 +0100

    Database Access (Refactored)

--- server/resolvers.js
+++ server/resolvers.js
@@ -2,10 +2,6 @@ import { getJobs } from './db/jobs.js'
 
 export const resolvers = {
   Query: {
-    jobs: async () => {
-      const jobs = await getJobs()
-      console.log(jobs)
-      return getJobs()
-    },
+    jobs: () => getJobs(),
   },
 }
