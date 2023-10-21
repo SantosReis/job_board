dc823308693f246807342e9a0235217a929288fc
Marcus Reis <mvsantosreis@gmail.com>
2023-10-21 07:51:08 +0100

    Arrays and Non-Nullability

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,11 +1,13 @@
 export const resolvers = {
   Query: {
-    job: () => {
-      return {
-        id: 'test-id',
-        title: 'The Title',
-        description: 'The description.',
-      }
+    jobs: () => {
+      return [
+        {
+          id: 'test-id 2',
+          title: 'The Title 2',
+          description: null,
+        },
+      ]
     },
   },
 }
