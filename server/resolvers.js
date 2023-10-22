6ee798a5f72476d25a8aaabbc6942ae5b7b575ce
Marcus Reis <mvsantosreis@gmail.com>
2023-10-22 20:07:03 +0100

    Custom Errors

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,9 +1,19 @@
+import { GraphQLError } from 'graphql'
 import { getCompany } from './db/companies.js'
 import { getJob, getJobs, getJobByCompany } from './db/jobs.js'
 
 export const resolvers = {
   Query: {
-    company: (_root, { id }) => getCompany(id),
+    //company: (_root, { id }) => getCompany(id),
+    company: async (_root, { id }) => {
+      const company = await getCompany(id)
+      if (!company) {
+        throw new GraphQLError('No Company found with id' + id, {
+          extensions: { code: 'NOT_FOUND' },
+        })
+      }
+      return company
+    },
     job: (_root, { id }) => getJob(id),
     jobs: () => getJobs(),
   },
