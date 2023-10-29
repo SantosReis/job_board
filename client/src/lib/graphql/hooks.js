38bea4bd1971ed440a2784c0796f7566f0888b81
Marcus Reis <mvsantosreis@gmail.com>
2023-10-29 19:04:21 +0000

    Custom Hook useCreateJob

--- client/src/lib/graphql/hooks.js
+++ client/src/lib/graphql/hooks.js
@@ -1,11 +1,41 @@
-import { useQuery } from '@apollo/client'
-import { companyByIdQuery, jobByIdQuery, jobsQuery } from './queries'
+import { useMutation, useQuery } from '@apollo/client'
+import {
+  companyByIdQuery,
+  createJobMutation,
+  jobByIdQuery,
+  jobsQuery,
+} from './queries'
 
 export function useCompany(id) {
   const { data, loading, error } = useQuery(companyByIdQuery, {
     variables: { id },
   })
   return { company: data?.company, loading, error: Boolean(error) }
+}
+
+export function useCreateJob() {
+  const [mutate, { loading }] = useMutation(createJobMutation)
+
+  const createJob = async (title, description) => {
+    const {
+      data: { job },
+    } = await mutate({
+      variables: { input: { title, description } },
+      update: (cache, { data }) => {
+        cache.writeQuery({
+          query: jobByIdQuery,
+          variables: { id: data.job.id },
+          data,
+        })
+      },
+    })
+    return job
+  }
+
+  return {
+    createJob,
+    loading,
+  }
 }
 
 export function useJob(id) {
